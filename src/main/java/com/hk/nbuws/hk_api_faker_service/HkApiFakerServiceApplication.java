7ac6324122717dc8e6de6bc197e0b2e5ba93d11b
pskfire <40130953+pskfire@users.noreply.github.com>
2025-12-21 12:40:57 +0800

    11

---
+++ src/main/java/com/hk/nbuws/hk_api_faker_service/HkApiFakerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hk.nbuws.hk_api_faker_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HkApiFakerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HkApiFakerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hk/nbuws/hk_api_faker_service/HkApiFakerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hk.nbuws.hk_api_faker_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HkApiFakerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
